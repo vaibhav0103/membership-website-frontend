189500f4c3ccb0b44a95e67b28f71878ad9ba5a5
vaibhav0103 <vaibhavsharma1393@gmail.com>
2021-05-31 20:25:18 +0530

    signup form validation

---
+++ src/components/InputFields.js
@@ -0,0 +1,28 @@
+const inputFieldValues = [
+  {
+    name: "username",
+    label: "Userame",
+    type: "text",
+    id: "username"
+  },
+  {
+    name: "email",
+    label: "Email",
+    type: "email",
+    id: "email"
+  },
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+    id: "password",
+  },
+  {
+    name: "confirmPassword",
+    label: "Confirm Password",
+    type: "password",
+    id: "confirmPassword",
+  },
+];
+
+export default inputFieldValues;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,6 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect} from 'react'; 
+import inputFieldValues from '../components/InputFields';
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
     const initialFormData = Object.freeze({
@@ -6,16 +8,68 @@ const Signup = () => {
 		username: '',
 		password: '',
 	});
+    const [errors, setErrors] = useState({})
     const [formData, setFormData] = useState(initialFormData)
 
+    const formIsValid = (fieldValues = formData) => {
+        const isValid =
+        fieldValues.username &&
+        fieldValues.email &&
+        fieldValues.password &&
+        Object.values(errors).every((x) => x === "");
+
+        return isValid;
+    };
+
+
+    const validateForm = (fieldValues = formData) => {
+
+        const temp = {...errors}
+
+        if ("username" in fieldValues)
+            temp.username = fieldValues.username ? "" : "This field is required.";
+
+        if ("email" in fieldValues) {
+            temp.email = fieldValues.email ? "" : "This field is required.";
+            if (fieldValues.email){
+                temp.email = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(fieldValues.email)
+                ? ""
+                : "Email is not valid.";
+            }
+        }
+
+        if ("password" in fieldValues){
+            temp.password = fieldValues.password.length >= 8 ? "" : "Must have atleast 8 characters.";
+        }
+
+        if ("confirmPassword" in fieldValues){
+            
+            if(formData.password){
+                console.log(formData.password, fieldValues.confirmPassword )
+                temp.confirmPassword = fieldValues.confirmPassword === formData.password ? "" : "Passwords do not match!";
+            } else{
+                temp.confirmPassword = fieldValues.confirmPassword.length < 1 ? "": "Enter a Password first";
+            }
+        }
+
+        setErrors({
+            ...temp
+        })
+
+    }
+
     const handleChange = (e) =>{
-        console.log(e.target.name)
-        console.log(e.target.value)
+        // Get User input
+        const { name, value } = e.target;
+        // Set Data
         setFormData({
 			...formData,
 			// Trimming any whitespace
-			[e.target.name]: e.target.value.trim(),
+			[name]: value.trim(),
 		});
+        // validate value
+        validateForm({[name]: value.trim()})
+        console.log(errors)
     }
 
     const handleSubmit = (e) => {
@@ -28,54 +82,46 @@ const Signup = () => {
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-sm-6">
-                    <form>
+                    <form noValidate>
                         <h3 className="text-primary text-center my-3">Sign Up</h3>
-                        {/* Username */}
-
-                        <div className="form-floating mb-3">
-                            <input type="text" className="form-control is-invalid" id="username" name="username" placeholder="Username" onChange={handleChange} />
-                            <label className="form-label text-danger" htmlFor="username">Username</label>
-                        </div>
-
-                        {/* Email input  */}
-                        <div className="form-floating mb-3">
-                            <input type="email" className="form-control is-valid" id="email" name="email" placeholder="name@example.com" onChange={handleChange} />
-                            <label className="form-label text-success" htmlFor="email">Email Address</label>
-                        </div>
-
-                        {/* Password input */}
-                        <div className="form-floating mb-3">
-                            <input type="password" className="form-control" id="password" name="password" placeholder="Password" onChange={handleChange} />
-                            <label className="form-label" htmlFor="password">Password</label>
-                        </div>
+                        {/* Input Fields */}
+                        {inputFieldValues.map((inputFieldValue, index) => {
+                            return (
+                                
+                                <div key={index} className="form-floating mb-3">
+                                    <input type={inputFieldValue.type} 
+                                    className={`form-control ${!!errors[inputFieldValue.name] ? "is-invalid" : "is-valid"} `} 
+                                    id={inputFieldValue.id} 
+                                    name={inputFieldValue.name} 
+                                    placeholder={inputFieldValue.label} 
+                                    onChange={handleChange} />
+                                    <label className={`form-label ${!!errors[inputFieldValue.name] ? "text-danger" : "text-success"}`} 
+                                    htmlFor={inputFieldValue.id}>
+                                        {inputFieldValue.label}
+                                    </label>
+                                    <div className="invalid-feedback"> 
+                                        {errors[inputFieldValue.name]}
+                                    </div>
+                                </div>
+                                
+                            )
+                        })}
 
                         {/*-- Submit button -- */}
                         <div className="d-grid gap-2 col-6 mx-auto">
-                            <button type="submit" className="btn btn-primary btn-block mb-2" onClick={handleSubmit}>Sign up</button>
+                            <button type="submit" 
+                            className={`btn btn-primary btn-block mb-2 ${!formIsValid() ? 'disabled' : ''} `} 
+                            onClick={handleSubmit}>
+                                Sign up
+                            </button>
                         </div>
-                        <div className="d-grid gap-2 col-6 mx-auto">
-                            Already a member? Sign In here...  
-                            <button type="submit" className="btn btn-info mb-2">Sign In</button>
+                        <div className="my-3 mx-auto">
+                            Already a member? 
+                            <Link to="/login" >
+                                <button type="button" className="btn btn-info mb-2 mx-2 p-2" > Login </button>
+                            </Link> 
                         </div>
-                        {/*-- Register buttons -- */}
-                        {/* <div className="text-center">
-                            <p>or sign up with:</p>
-                            <button type="button" className="btn btn-primary btn-floating mx-1">
-                            <i className="fab fa-facebook-f"></i>
-                            </button>
-
-                            <button type="button" className="btn btn-primary btn-floating mx-1">
-                            <i className="fab fa-google"></i>
-                            </button>
-
-                            <button type="button" className="btn btn-primary btn-floating mx-1">
-                            <i className="fab fa-twitter"></i>
-                            </button>
-
-                            <button type="button" className="btn btn-primary btn-floating mx-1">
-                            <i className="fab fa-github"></i>
-                            </button>
-                        </div> */}
+                        
                     </form>
                 </div>
             </div>
